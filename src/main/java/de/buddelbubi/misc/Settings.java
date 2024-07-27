bd6f652031091e3a3ba1d922319c8c2f763bed18
Buddelbubi <44944269+Buddelbubi@users.noreply.github.com>
2024-07-27 14:58:42 +0200

    Update Settings.java

--- src/main/java/de/buddelbubi/misc/Settings.java
+++ src/main/java/de/buddelbubi/misc/Settings.java
@@ -10,20 +10,19 @@ import java.awt.event.KeyEvent;
 
 public class Settings {
 
+    //Do not change this. This is a switch to block new actions if another action is running.
     public static boolean IN_ACTION = false;
+
     public static boolean ENABLED = true;
-    public static boolean AUTOMATIC_JETPACK_REFILL = true;
     public static boolean AUTOMATIC_GARAGE = true;
     public static boolean AUTOMATIC_LOCK = true;
     public static int STATUS_SIZE = 30;
     public static int ZOOM_SCROLL_AMOUNT = 700;
 
     public static boolean toggleEnable() {
-
         ENABLED = !ENABLED;
         UI.STATUS.getContentPane().setBackground(ENABLED ? Color.GREEN : Color.RED);
         return ENABLED;
-
     }
 
 }
