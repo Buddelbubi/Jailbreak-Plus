380f0d525c32cfa7c44edf6d55a46a3741cd89ea
Buddelbubi <44944269+Buddelbubi@users.noreply.github.com>
2024-07-27 14:58:27 +0200

    Update Casino.java

--- src/main/java/de/buddelbubi/display/robberies/Casino.java
+++ src/main/java/de/buddelbubi/display/robberies/Casino.java
@@ -12,7 +12,6 @@ import java.util.concurrent.TimeUnit;
 
 public class Casino {
 
-    private static int clicked = 0;
     private static long lastClick = 0;
 
     public static void init() {
@@ -26,20 +25,17 @@ public class Casino {
                             Point end = ScreenReader.calculateElementPos(0.5, 0.7);
                             Rectangle captureRect = new Rectangle(start.x, start.y, end.x, end.y- 36);
                             BufferedImage capture = ScreenReader.getROBOT().createScreenCapture(captureRect);
-                            boolean found = false;
                             for (int x = 0; x < capture.getWidth(); x++) {
                                 for (int y = 0; y < capture.getHeight(); y++) {
-                                    if (new Color(capture.getRGB(x, y)).equals(new Color(0, 255, 1))) {
-
+                                    Color color = new Color(capture.getRGB(x, y));
+                                    if (color.equals(new Color(0, 255, 1))) {
                                         long diff = System.currentTimeMillis() - lastClick;
                                         lastClick = System.currentTimeMillis();
-                                        if (clicked < 3) {
-                                            if (diff < 8000) {
-                                                clicked++;
-                                                ScreenReader.getROBOT().keyPress(KeyEvent.VK_E);
-                                                ScreenReader.getROBOT().keyRelease(KeyEvent.VK_E);
-                                            }
-                                        } else clicked = 0;
+                                        if (diff < 500) {
+                                            ScreenReader.getROBOT().keyPress(KeyEvent.VK_E);
+                                            ScreenReader.getROBOT().keyRelease(KeyEvent.VK_E);
+                                            break;
+                                        }
                                     }
                                 }
                             }
